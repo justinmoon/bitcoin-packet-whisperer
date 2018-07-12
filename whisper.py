114a72ed2defa237307df8223fc80aa8b8554a4b
Justin Moen <jamoen7@gmail.com>
2018-07-12 10:52:19 -0500

    hacks

--- whisper.py
+++ whisper.py
@@ -2,9 +2,14 @@
 Adapted from:
 * https://github.com/petertodd/python-bitcoinlib/blob/master/examples/send-addrs-msg.py
 * https://github.com/jimmysong/pb-exercises/blob/master/session7/helper.py
+
+Notes:
+* It doesn't seem like python-bitcoinlib can really read incoming messages ...
+* It's very annoying how python-bitcoinlib's "msg" objects have arbitrarily named data attributes.
+Sometimes it's `msg.tx` or `msg.addr` or msg.inv` ...
 """
 import socket, time, bitcoin, hashlib
-from bitcoin.messages import msg_version, msg_verack, msg_addr, msg_getdata
+from bitcoin.messages import msg_version, msg_verack, msg_addr, msg_getdata, MsgSerializable
 from bitcoin.net import CAddress, CInv
 
 
@@ -14,33 +19,8 @@ NETWORK_MAGIC = b'\xf9\xbe\xb4\xd9'
 bitcoin.SelectParams('mainnet') 
 
 
-def little_endian_to_int(b):
-    return int.from_bytes(b, 'little')
-
-
-def int_to_little_endian(n, length):
-    return n.to_bytes(length, 'little')
-
-
-def double_sha256(s):
-    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
-
-
-def read_varint(s):
-    # https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer
-    i = s.read(1)[0]
-    if i == 0xfd:
-        # 0xfd means the next two bytes are the number
-        return little_endian_to_int(s.read(2))
-    elif i == 0xfe:
-        # 0xfe means the next four bytes are the number
-        return little_endian_to_int(s.read(4))
-    elif i == 0xff:
-        # 0xff means the next eight bytes are the number
-        return little_endian_to_int(s.read(8))
-    else:
-        # anything else is just the integer
-        return i
+txns = []
+addrs = []
 
 
 def version_pkt(client_ip, server_ip):
@@ -68,23 +48,11 @@ def addr_pkt( str_addrs ):
     msg.addrs = addrs
     return msg
 
-def getdata_pkt( inv ):
-    # https://en.bitcoin.it/wiki/Protocol_documentation#getdata
+def getdata_pkt( inv_vec ):
+    # so annoying how this lib doesn't use constructors ...
     msg = msg_getdata()
-    for type_, hash_ in inv:
-        cinv = CInv()
-        cinv.type = type_
-        cinv.hash = hash_
-        msg.inv.append(cinv)
+    msg.inv = inv_vec
     return msg
-
-
-def read_inv(stream):
-    # https://en.bitcoin.it/wiki/Protocol_documentation#inv
-    type_ = little_endian_to_int(stream.read(4))
-    hash_ = stream.read(32)
-    return (type_, hash_)
-
 
 def connect():
     s = socket.socket()
@@ -98,68 +66,65 @@ def connect():
 
     # Send Version packet
     s.send( version_pkt(client_ip, server_ip).to_bytes() )
+    print('Sent "ver" message')
 
     # Get Version reply
-    # TODO: Should we do something with it?
-    # TODO: Print something useful
-    print(s.recv(1924))
+    # TODO: Should we do something with it? How to read it?
+    ver = s.recv(1924)
+    print('Received "ver" message')
 
     # Send Verack
     # https://en.bitcoin.it/wiki/Protocol_documentation#verack
     s.send( msg_verack().to_bytes() )
 
     # Get Verack
-    # TODO: Should we do something with it?
-    print(s.recv(1024))
+    # TODO: Should we do something with it? How to read it?
+    verack = s.recv(1024)
+    print('Received "verack" message')
 
     # Send Addrs
     # FIXME: what address is this?
     s.send( addr_pkt(["252.11.1.2", "EEEE:7777:8888:AAAA::1"]).to_bytes() )
+    print('Sent "verack" message')
+    
     return s
 
 
 def main_loop(s):
+    iterations = 0
+
     while True:
-        data = s.recv(1024)
-        import io
-        stream = io.BytesIO(data)
-
-        # FIXME: write helper function
-        # https://en.bitcoin.it/wiki/Protocol_documentation#Message_structure
-        magic = stream.read(4)
-        if magic != NETWORK_MAGIC:
-            # FIXME broken
-            print(f"{magic} != {NETWORK_MAGIC}")
-            print()
-            continue
-            #raise RuntimeError('magic is not right')
-        command = stream.read(12)
-        payload_length = little_endian_to_int(stream.read(4))
-        checksum = stream.read(4)
-        payload = stream.read(payload_length)
-        calculated_checksum = double_sha256(payload)[:4]
-
-        # FIXME broken
-        # print(calculated_checksum, checksum)
-        # if calculated_checksum != checksum:
-        #     raise RuntimeError('checksum does not match')
-
-        # FIXME: write handle_inv()
-        if command.startswith(b'inv'):
-            inv_stream = io.BytesIO(payload)
-            count = read_varint(inv_stream)
-            inv_vec = []
-            for _ in range(count):
-                inv_vec.append(read_inv(inv_stream))
-            pkt = getdata_pkt(inv_vec)
-            print('sending inv packet')
-            s.send(pkt.to_bytes())
-
-            
-
-        print(command)
-        #print(payload)
         print()
+
+        data = s.recv(1024* 100)
+
+        try:
+            # FIXME: this is broken. Can I just stream from the socket???
+            msg = MsgSerializable.from_bytes(data)
+        except Exception as e:
+            print(f'Message deserialization failed: {e}')
+            continue
+
+        if msg.command == b'inv':
+            # https://en.bitcoin.it/wiki/Protocol_documentation#getdata
+            # msg.inv is actually an inv_vec
+            m = getdata_pkt(msg.inv)
+            s.send(m.to_bytes())
+            print(f'Sent "inv" message: {msg.inv}')
+
+        if msg.command == b'tx':
+            txns.append(msg.tx)
+            print(f'Received "tx": {msg.tx}')
+
+        if msg.command == b'addr':
+            addrs.extend(msg.addrs)
+            print(f'Received "addrs": {msg.addrs}')
+
+        # HACK
+        iterations += 1
+        if iterations % 10 == 0:
+            print(f"#txns: {len(txns)}")
+            print(f"#addrs: {len(addrs)}")
 
 
 def main():
